6fa0dbb6674f0eb14fb242e36930b9691b047440
Alex Ehlke <alex.ehlke@gmail.com>
2017-07-19 13:10:42 -0400

    add fix for iOS 11
    See http://www.openradar.me/32599304

---
+++ SwiftReadability/String+MalformedEncoding.swift
@@ -0,0 +1,30 @@
+//
+//  String+MalformedUnicode.swift
+//  SwiftReadability
+//
+//  Created by Alex Ehlke on 2017-07-08.
+//  Copyright © 2017 Chloe Horgan. All rights reserved.
+//
+
+import Foundation
+
+// https://stackoverflow.com/a/44611946/89373
+extension String {
+    init(malformedUTF8 data: Data) {
+        var str = ""
+        var iterator = data.makeIterator()
+        var utf8codec = UTF8()
+        var done = false
+        while !done {
+            switch utf8codec.decode(&iterator) {
+            case .emptyInput:
+                done = true
+            case let .scalarValue(val):
+                str.unicodeScalars.append(val)
+            case .error:
+                break // ignore errors
+            }
+        }
+        self = str
+    }
+}
